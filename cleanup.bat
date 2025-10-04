4c13b2b8432c6466e298bbbeb2eda1b46141d830
John Eley <johneley798@gmail.com>
2025-10-05 06:51:00 +1000

    Removed cleanup.bat(unused)

--- cleanup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Removing non-English satellite assemblies...
-for /d %%i in (publish\*) do (
-    if /i not "%%~ni" == "en" (
-        if exist %%i\*.resources.dll rmdir /s /q "%%i"
-    )
-)
-echo Done.
